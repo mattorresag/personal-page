3f3cc44e1274ddc41cf5329607562defa4d61c55
Matheus <matheustorres@poli.ufrj.br>
2024-07-11 14:07:23 -0300

    chore: install project and tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,29 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        figtree: ["Figtree", "sans-serif"],
+        mPlusCodeLatin: ["M PLUS Code Latin", "monospace"],
+      },
+      colors: {
+        neutral: {
+          10: "#E8E8EA",
+          40: "#8D8B9D",
+          60: "#B6B4C0",
+        },
+        primary: {
+          pure: "#4341E1",
+          10: "#A594FD",
+          20: "rgb(165, 148, 253,0.2)",
+        },
+      },
+      borderRadius: {
+        md: "0.5rem",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
