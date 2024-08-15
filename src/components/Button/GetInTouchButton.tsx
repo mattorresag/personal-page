dc46580fa3b92e1e9b84887c68f0c7e6b6c9c799
Matheus <matheustorres@poli.ufrj.br>
2024-08-14 21:48:24 -0300

    fix: anchor tag taking whole flex width

--- src/components/Button/GetInTouchButton.tsx
+++ src/components/Button/GetInTouchButton.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 export const GetInTouchButton = () => {
   return (
-    <a href="mailto:matheusftaguiar@gmail.com?subject=Budget a project">
+    <a
+      href="mailto:matheusftaguiar@gmail.com?subject=Budget a project"
+      className="w-fit"
+    >
       <button className="bg-primary-pure h-14 w-[210px] lg:w-[238px] rounded-tl-lg rounded-br-lg">
         <p className="text-white font-[700] subheading12 font-figtree tracking-[3px]">
           contact me
