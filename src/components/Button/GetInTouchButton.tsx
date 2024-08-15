afbb99ca8f7a1fbf225b421d4eff06f76d0056f4
Matheus <matheustorres@poli.ufrj.br>
2024-08-14 22:15:15 -0300

    refac: button edges

--- src/components/Button/GetInTouchButton.tsx
+++ src/components/Button/GetInTouchButton.tsx
@@ -4,9 +4,16 @@ export const GetInTouchButton = () => {
   return (
     <a
       href="mailto:matheusftaguiar@gmail.com?subject=Budget a project"
-      className="w-fit"
+      className="w-fit  bg-opacity-50"
     >
-      <button className="bg-primary-pure h-14 w-[210px] lg:w-[238px] rounded-tl-lg rounded-br-lg">
+      <button
+        className="bg-primary-pure/80 hover:bg-primary-pure/10 transition ease-in duration-200 h-14 w-[210px] lg:w-[210px]"
+        style={{
+          clipPath:
+            "polygon(5.3% 0, 100% 0, 100% 80%, 94.7% 100%, 0 100%, 0 20%)",
+          boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+        }}
+      >
         <p className="text-white font-[700] subheading12 font-figtree tracking-[3px]">
           contact me
         </p>
