25db6ef907de9d9caecc686fd63de3feba6616af
Matheus <matheustorres@poli.ufrj.br>
2024-09-05 21:28:47 -0300

    refac: meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,9 @@ import "./globals.css";
 const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Matheus Torres Dev",
+  title: "Matheus Torres | FullStack Developer",
   description:
-    "FullStack Developer with skills in React, Next.js, Node.js, and more.",
+    "FullStack Developer with skills in React, Next.js, Node.js, and more. I am a professional who transforms design into web pages with exceptional speed, quality, and performance, bringing your project to life. Contact me for a free consultation.",
 };
 
 export default function RootLayout({
