7fd12e9553204046e9b4d135cfece8d9040083ac
Matheus Torres <matheustorres@poli.ufrj.br>
2024-09-30 17:47:37 -0300

    refac: robots and sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "/",
+      url: "https://matheusftagdev.vercel.app/",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
