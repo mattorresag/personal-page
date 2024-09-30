3d2e50c19d6e2013e295e441b8112157683cbbf3
Matheus Torres <matheustorres@poli.ufrj.br>
2024-09-30 18:01:37 -0300

    refac: openGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,13 @@ export const metadata: Metadata = {
   title: "Matheus Torres | FullStack Developer",
   description:
     "I am Matheus Torres, a FullStack Developer with skills in React, Next.js, Node.js, and more. I am a professional who transforms design into web pages with exceptional speed, quality, and performance.",
+  openGraph: {
+    title: "Matheus Torres | FullStack Developer",
+    description:
+      "FullStack Developer specializing in React, Next.js, Node.js, and more.",
+    type: "website",
+    url: "https://matheusftagdev.vercel.app/",
+  },
 };
 
 export default function RootLayout({
